33da3a04e4fcd8c1dd3bceb8c9b7bc0bb042c052
Jack Sundholm <sundholm38@gmail.com>
2024-10-31 19:22:44 +0200

    US3 green

--- piglatin.py
+++ piglatin.py
@@ -10,4 +10,14 @@ class PigLatin:
     def translate(self) -> str:
         if self.phrase == "":
             return "nil"
+
+        vowels = "aeiouAEIOU"
+        if len(self.phrase.split()) == 1 and self.phrase[0] in vowels:
+            if self.phrase[-1] == "y":
+                return self.phrase + "nay"
+            elif self.phrase[-1] in vowels:
+                return self.phrase + "yay"
+            else:
+                return self.phrase + "ay"
+
         return ""
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -12,5 +12,15 @@ class TestPigLatin(unittest.TestCase):
         translator = PigLatin("")
         self.assertEqual(translator.translate(), "nil")
 
+    def test_translate_word_starting_with_vowel(self):
+        translator_any = PigLatin("any")
+        self.assertEqual(translator_any.translate(), "anynay")
+
+        translator_apple = PigLatin("apple")
+        self.assertEqual(translator_apple.translate(), "appleyay")
+
+        translator_ask = PigLatin("ask")
+        self.assertEqual(translator_ask.translate(), "askay")
+
 if __name__ == "__main__":
     unittest.main()
