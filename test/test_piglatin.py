00adb9ddb7fe64ad75199344e7ab971302bb64d4
Jack Sundholm <sundholm38@gmail.com>
2024-10-31 19:18:43 +0200

    US2 green

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -8,5 +8,9 @@ class TestPigLatin(unittest.TestCase):
         translator = PigLatin("hello world")
         self.assertEqual(translator.get_phrase(), "hello world")
 
+    def test_translate_empty_phrase(self):
+        translator = PigLatin("")
+        self.assertEqual(translator.translate(), "nil")
+
 if __name__ == "__main__":
     unittest.main()
