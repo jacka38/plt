d5b5fd9fc1e5be8a0839605ffd8c4f7ec2cb361d
Jack Sundholm <sundholm38@gmail.com>
2024-10-31 19:10:48 +0200

    US1 green

--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,9 +1,12 @@
+
 import unittest
 from piglatin import PigLatin
-from error import PigLatinError
-
 
 class TestPigLatin(unittest.TestCase):
 
-    def test_something(self):
-        pass
+    def test_get_phrase(self):
+        translator = PigLatin("hello world")
+        self.assertEqual(translator.get_phrase(), "hello world")
+
+if __name__ == "__main__":
+    unittest.main()
