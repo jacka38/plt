462831fddf479714c29020701d5cfed7b3e326d4
Jack Sundholm <sundholm38@gmail.com>
2024-11-01 13:49:40 +0200

    US7 green

--- piglatin.py
+++ piglatin.py
@@ -1,6 +1,8 @@
 
-class PigLatin:
+from error import PigLatinError
 
+
+class PigLatin:
     def __init__(self, phrase: str):
         self.phrase = phrase
 
@@ -11,14 +13,32 @@ class PigLatin:
         if self.phrase == "":
             return "nil"
 
+        allowed_punctuation = {'.', ',', ';', ':', "'", '?', '!', '(', ')'}
         translated_words = []
         words = self.phrase.split()
 
         for word in words:
-            composite_words = word.split('-')
+            core_word = ""
+            trailing_punctuation = ""
+            for i in range(len(word)):
+                if word[-(i+1)] in allowed_punctuation:
+                    trailing_punctuation = word[-(i+1)] + trailing_punctuation
+                else:
+                    core_word = word[:len(word)-i]
+                    break
+
+            for char in core_word:
+                if not char.isalpha() and char != '-':
+                    raise PigLatinError(f"Invalid punctuation: '{char}'")
+
+            composite_words = core_word.split('-')
             translated_composite = []
 
             for composite_word in composite_words:
+                if not composite_word:
+                    translated_composite.append(composite_word)
+                    continue
+
                 vowels = "aeiouAEIOU"
 
                 if composite_word[0] in vowels:
@@ -28,6 +48,7 @@ class PigLatin:
                         translated_composite.append(composite_word + "yay")
                     else:
                         translated_composite.append(composite_word + "ay")
+
                 else:
                     consonant_cluster = ""
                     for char in composite_word:
@@ -35,8 +56,10 @@ class PigLatin:
                             break
                         consonant_cluster += char
 
-                    translated_composite.append(composite_word[len(consonant_cluster):] + consonant_cluster + "ay")
+                    translated_core = composite_word[len(consonant_cluster):] + consonant_cluster + "ay"
+                    translated_composite.append(translated_core)
 
-            translated_words.append('-'.join(translated_composite))
+            translated_word = '-'.join(translated_composite) + trailing_punctuation
+            translated_words.append(translated_word)
 
         return ' '.join(translated_words)
--- test/test_piglatin.py
+++ test/test_piglatin.py
@@ -1,5 +1,7 @@
 
 import unittest
+
+from error import PigLatinError
 from piglatin import PigLatin
 
 
@@ -37,6 +39,18 @@ class TestPigLatin(unittest.TestCase):
 
         translator_well_being = PigLatin("well-being")
         self.assertEqual(translator_well_being.translate(), "ellway-eingbay")
+
+    def test_translate_phrase_with_punctuation(self):
+        translator_hello_world_exclamation = PigLatin("hello world!")
+        self.assertEqual(translator_hello_world_exclamation.translate(), "ellohay orldway!")
+
+        translator_with_punctuation = PigLatin("well-being, good day.")
+        self.assertEqual(translator_with_punctuation.translate(), "ellway-eingbay, oodgay ayday.")
+
+    def test_translate_invalid_punctuation(self):
+        with self.assertRaises(PigLatinError):
+            translator_invalid = PigLatin("hello world@")
+            translator_invalid.translate()
 
 
 if __name__ == "__main__":
